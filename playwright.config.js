3442484169d055a88e25432041106ee740a034a3
victorikeokoro4 <victor.qa@code-care.pro>
2023-03-14 10:23:11 +0100

    pdp demo

---
+++ playwright.config.js
@@ -0,0 +1,48 @@
+// @ts-check
+const { devices } = require('@playwright/test');
+
+const config = {
+  testDir: './tests',
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+
+  expect: {
+    timeout: 5000
+  },
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [
+    ["list"],
+    [
+      "json",
+      {
+        outputFile: "../report/test-results.json",
+      },
+    ],
+    [
+      "html",
+      {
+        open: "always",
+        host: "localhost",
+        port: 9223,
+      },
+    ],
+  ],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
+    headless:false,
+    trace: 'on',
+    browserName:"chromium"
+  },
+};
+
+module.exports = config;
